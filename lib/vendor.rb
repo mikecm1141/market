38b22bb3124bd684b1e554b67f0ad79e815deb7b
Mike McKee <mikecm@gmail.com>
2018-07-31 09:16:33 -0600

    Finishing iteration 1 with vendor class and tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,9 +2,19 @@
 
 # Vendor class
 class Vendor
-  attr_reader :name
+  attr_reader :name,
+              :inventory
 
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
   end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -21,4 +21,54 @@ class VendorTest < Minitest::Test
 
     assert_equal expected, actual
   end
+
+  def test_inventory_hash_starts_empty
+    assert_empty @vendor.inventory
+  end
+
+  def test_stock_can_be_checked_and_default_is_zero
+    expected = 0
+    actual   = @vendor.check_stock('Peaches')
+
+    assert_equal expected, actual
+  end
+
+  def test_stock_can_be_added_to
+    expected = 30
+    actual   = @vendor.stock('Peaches', 30)
+
+    assert_equal expected, actual
+
+    expected2 = 30
+    actual2   = @vendor.check_stock('Peaches')
+
+    assert_equal expected2, actual2
+
+    expected3 = 55
+    actual3   = @vendor.stock('Peaches', 25)
+
+    assert_equal expected3, actual3
+
+    expected4 = 55
+    actual4   = @vendor.check_stock('Peaches')
+
+    assert_equal expected4, actual4
+  end
+
+  def test_it_can_have_different_items
+    expected = 12
+    actual   = @vendor.stock('Tomatoes', 12)
+
+    assert_equal expected, actual
+  end
+
+  def test_its_inventory_hash_stores_stock
+    @vendor.stock('Peaches',  55)
+    @vendor.stock('Tomatoes', 12)
+
+    expected = { 'Peaches' => 55, 'Tomatoes' => 12 }
+    actual   = @vendor.inventory
+
+    assert_equal expected, actual
+  end
 end
